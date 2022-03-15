1ce97729d90e018b4cadc9ba44f2f7c482c69211
haydar-99 <haydaralsaadi33@gmail.com>
2022-03-15 21:39:25 +0100

    coneccted to AtlasDB-Dataabase

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require("express")
-
-
-
-
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const env = require('dotenv').config()
 const app = express()
 
 
-app.listen(5000, ()=>{
-    console.log("running on port 5000")
+
+
+mongoose
+.connect(process.env.DATABASE_URL)
+.then(()=>{console.log("connected to the database")
+app.listen(5000, ()=>{console.log("running on port 5000")})
 })
+.catch(err=>{console.log(err.message)})
+
